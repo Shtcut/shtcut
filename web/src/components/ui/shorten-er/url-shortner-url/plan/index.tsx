7c321579c4c5653fb42ba745efd3879a17b56d0b
funmiayinde <funmiayinde11@gmail.com>
2024-05-03 01:18:12 +0100

    fix key issues

--- web/src/components/ui/shorten-er/url-shortner-url/plan/index.tsx
+++ web/src/components/ui/shorten-er/url-shortner-url/plan/index.tsx
@@ -5,7 +5,6 @@ import { PLANS } from '@shtcut/_shared/data';
 import PlanTab from '@shtcut/components/tabs';
 import { Check } from 'lucide-react';
 
-
 import ConfettiExplosion, { ConfettiProps } from 'react-confetti-explosion';
 
 const mediumProps: ConfettiProps = {
@@ -16,15 +15,11 @@ const mediumProps: ConfettiProps = {
     colors: ['#9A0023', '#FF003C', '#AF739B', '#FAC7F3', '#F7DBF4']
 };
 
-
-
-
 const PlanShorten = () => {
-     const [isExploding, setIsExploding] = useState(false);
-      console.log('isexplode', isExploding);
+    const [isExploding, setIsExploding] = useState(false);
+    console.log('isexplode', isExploding);
     return (
         <div className="max-w-screen-xl px-4  mx-auto">
-          
             <section className="flex flex-col gap-6">
                 <h2
                     className="font-heading text-2xl drop-shadow-xl dark:bg-gradient-to-br dark:from-neutral-200 dark:to-neutral-600 dark:bg-clip-text dark:text-transparent sm:text-3xl md:text-6xl text-center"
@@ -33,7 +28,6 @@ const PlanShorten = () => {
                     Choose Plan
                 </h2>
 
-              
                 <h2 className="font-heading text-2xl drop-shadow-xl dark:bg-gradient-to-br dark:from-neutral-200 dark:to-neutral-600 dark:bg-clip-text dark:text-transparent sm:text-3xl md:text-6xl text-center ">
                     That’s Right For You
                 </h2>
@@ -50,7 +44,10 @@ const PlanShorten = () => {
             </section>
             <section className="grid mb-10 sm:grid-cols-2 lg:grid-cols-3    sm:w-5/6 mx-auto gap-4 ">
                 {PLANS.map((_e) => (
-                    <div className="w-full shadow-sm border p-4 md:p-8  rounded-xl hover:bg-white transition-transform duration-200 hover:-translate-y-2 cursor-pointer">
+                    <div
+                        key={_e.id}
+                        className="w-full shadow-sm border p-4 md:p-8  rounded-xl hover:bg-white transition-transform duration-200 hover:-translate-y-2 cursor-pointer"
+                    >
                         <div className="flex flex-col gap-1">
                             <h1 className="font-bold text-xl">{_e.status}</h1>
                             <div className="flex items-center gap-2">
@@ -60,8 +57,8 @@ const PlanShorten = () => {
                             <p className="md:w-6/7">{_e.text}</p>
                         </div>
                         <section className={`flex flex-col gap-3 ${_e.id === 1 ? 'my-12' : 'my-6'}`}>
-                            {_e.data.map((str) => (
-                                <div className="flex  gap-2">
+                            {_e.data.map((str, idx) => (
+                                <div key={`${str}-${idx}`} className="flex  gap-2">
                                     <div className="w-5 h-5 rounded-full bg-tertiary-300 flex justify-center items-center font-semibold text-white relative top-0.5">
                                         <Check size={'12px'} color="white" className="font-bold" />
                                     </div>
@@ -77,11 +74,7 @@ const PlanShorten = () => {
                                     _e.id === 1 ? 'border border-gray-100' : ''
                                 } font-semibold hover:text-white`}
                             >
-                                {_e.id === 1
-                                    ? 'Signup for free '
-                                    : _e.id === 2
-                                      ? 'Go to pro'
-                                      : 'Start Free Trial'}
+                                {_e.id === 1 ? 'Signup for free ' : _e.id === 2 ? 'Go to pro' : 'Start Free Trial'}
                             </Button>
                             {_e.id === 3 && (
                                 <Button className=" bg-white rounded-full w-full border-primary-0 text-primary-0 border hover:text-white h-12 font-semibold">
