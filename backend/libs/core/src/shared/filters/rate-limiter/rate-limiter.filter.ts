b18de4d0bea0a0bc82d5412a2741a1305f75e184
funmiayinde <funmiayinde11@gmail.com>
2024-05-07 20:56:30 +0100

    working on rate limiter

--- backend/libs/core/src/shared/filters/rate-limiter/rate-limiter.filter.ts
+++ backend/libs/core/src/shared/filters/rate-limiter/rate-limiter.filter.ts
@@ -1,16 +1,23 @@
-import { RateLimiterError } from 'shtcut/core';
-import { BaseExceptionFilter } from '@nestjs/core';
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { AppException, RateLimiterError, setHeaders } from 'shtcut/core';
+import { APP_FILTER, BaseExceptionFilter } from '@nestjs/core';
+import { ArgumentsHost, Catch, ClassProvider, ExceptionFilter } from '@nestjs/common';
 import { ServerResponse } from 'http';
 
 
 @Catch(RateLimiterError)
-export class RateLimiterFilter extends BaseExceptionFilter implements ExceptionFilter{
+export class RateLimiterFilter extends BaseExceptionFilter implements ExceptionFilter {
 
-  catch(exception: any, host: ArgumentsHost) {
+  catch(exception: RateLimiterError, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
     const nativeResponse: ServerResponse = response.raw || response;
 
-    return super.catch(exception, host);
+    setHeaders(nativeResponse, exception.limiterInfo);
+    nativeResponse.setHeader('Retry-After', (exception.limiterInfo.reset - Date.now() / 1000) | 0);
+    return super.catch(AppException.TOO_MANY_REQUEST(exception.message), host);
   }
 }
+
+export const rateLimiterProvider: ClassProvider<ExceptionFilter> = {
+  provide: APP_FILTER,
+  useClass: RateLimiterFilter,
+};
