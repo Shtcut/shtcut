868a468a724bbe5b23695a4a80b3775bf2982f17
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2024-06-19 18:56:09 +0100

    workspace and auth ui design implementation and logic flows

--- web/src/components/ui/auth/auth-tab/index.tsx
+++ web/src/components/ui/auth/auth-tab/index.tsx
@@ -40,6 +40,7 @@ export const AuthTabs = () => {
             options: { noSuccessMessage: true }
         });
     };
+
     const ErrorAlert = ({ message }: { message: string }) => (
         <AppAlert
             variant="destructive"
