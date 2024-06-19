8e66e68442f1981b755dc90e4e10b2442ae3023f
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2024-06-19 19:50:05 +0100

    update on review

--- web/src/components/form/auth/sign-in-form/index.tsx
+++ web/src/components/form/auth/sign-in-form/index.tsx
@@ -1,8 +1,19 @@
 'use client';
 
-import { Dict, Form, FormControl, FormField, FormItem, Label, FormMessage, Input, cn } from '@shtcut-ui/react';
+import {
+    Dict,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    Label,
+    FormMessage,
+    Input,
+    cn,
+    Checkbox
+} from '@shtcut-ui/react';
 import { NavLink } from '@shtcut/components';
-import { AppAlert, AppButton, PasswordInput } from '@shtcut/components/_shared';
+import { AppButton, PasswordInput } from '@shtcut/components/_shared';
 import { get } from 'lodash';
 import { HTMLAttributes } from 'react';
 import { useForm } from 'react-hook-form';
@@ -35,18 +46,18 @@ export const SignInForm = (props: SignInFormProps) => {
     });
 
     return (
-        <div className={cn('grid gap-6', className)} {...props}>
+        <div className="w-full mt-8">
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(handleFormSubmit)}>
-                    <div className="grid gap-2">
+                    <div className="flex flex-col gap-3 w-full">
                         <FormField
                             control={form.control}
                             name="email"
                             render={({ field }) => (
-                                <FormItem className="space-y-1">
-                                    <Label>Email</Label>
+                                <FormItem className="space-y-1  mb-2 w-full">
+                                    <Label>Email address</Label>
                                     <FormControl>
-                                        <Input placeholder="name@example.com" className="h-12" {...field} />
+                                        <Input placeholder="Enter your email" className="h-12 w-full" {...field} />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
@@ -56,53 +67,50 @@ export const SignInForm = (props: SignInFormProps) => {
                             control={form.control}
                             name="password"
                             render={({ field }) => (
-                                <FormItem className="space-y-1">
-                                    <div className="flex items-center justify-start">
+                                <FormItem className="space-y-1 w-full">
+                                    <div className="flex mb-2 items-center justify-start">
                                         <Label>Password</Label>
                                     </div>
                                     <FormControl>
-                                        <PasswordInput className="h-12" placeholder="********" {...field} />
+                                        <PasswordInput className="h-12" placeholder="Must be 8 characters" {...field} />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
                             )}
                         />
-                        <div className="text-sm mt-2 font-medium text-muted-foreground">
-                            Forgot password?
-                            <NavLink
-                                href="/auth/forgot-password"
-                                className="px-1  hover:opacity-75 text-blue-600 hover:text-blue-500"
-                            >
-                                Click here
-                            </NavLink>
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center gap-x-3">
+                                <Checkbox />
+                                <p className="text-sm text-[#334155]">Keep me logged in</p>
+                            </div>
+                            <div className="text-sm  ">
+                                <NavLink
+                                    href="/auth/forgot-password"
+                                    className=" hover:opacity-75 text-primary-0 hover:text-blue-500"
+                                >
+                                    Forgot password?
+                                </NavLink>
+                            </div>
                         </div>
                         <AppButton
-                            className="mt-2 h-12 px-4 py-2 text-white font-medium bg-blue-600 hover:bg-blue-500 active:bg-blue-600 rounded-lg duration-150"
+                            className="mt-10 h-12 px-4 py-2 text-white font-semibold bg-blue-600 hover:bg-blue-500 active:bg-blue-600 rounded-lg duration-150"
                             loading={isLoading}
                         >
-                            Sign in
+                            Sign In
                         </AppButton>
 
-                        <div className="relative my-2">
+                        <div className="relative mt-3 mb-6">
                             <div className="absolute inset-0 flex items-center">
                                 <span className="w-full border-t" />
                             </div>
                             <div className="relative flex justify-center text-xs uppercase">
-                                <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
+                                <span className="bg-background px-2 text-muted-foreground">Or </span>
                             </div>
                         </div>
                     </div>
                 </form>
             </Form>
             <SocialLogin onFailure={onFailure} onSuccess={onSuccess} isLoading={isLoading} />
-            <div className="text-center">
-                <Label className="text-muted-foreground">
-                    Don`t have account?
-                    <NavLink href="/auth/sign-up" className="px-1 text-blue-600 hover:opacity-75 hover:text-blue-500">
-                        Sign up
-                    </NavLink>
-                </Label>
-            </div>
         </div>
     );
 };
