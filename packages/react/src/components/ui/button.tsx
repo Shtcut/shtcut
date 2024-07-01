cb138b80b38681e0c64c5f1adb74a47902401872
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2024-07-01 11:13:57 +0100

    ui design on dashboard

--- packages/react/src/components/ui/button.tsx
+++ packages/react/src/components/ui/button.tsx
@@ -14,20 +14,21 @@ const buttonVariants = cva(
                 outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
                 secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
                 ghost: 'hover:bg-accent hover:text-accent-foreground',
-                link: 'text-primary underline-offset-4 hover:underline'
+                link: 'text-primary underline-offset-4 hover:underline',
+                unstyled: ' border-none focus:border-none focus-visible:ring-0 focus-visible:ring-ring',
             },
             size: {
                 default: 'h-9 px-4 py-2',
                 sm: 'h-8 rounded-md px-3 text-xs',
                 lg: 'h-10 rounded-md px-8',
-                icon: 'h-9 w-9'
-            }
+                icon: 'h-9 w-9',
+            },
         },
         defaultVariants: {
             variant: 'default',
-            size: 'default'
-        }
-    }
+            size: 'default',
+        },
+    },
 );
 
 export interface ButtonProps
@@ -40,7 +41,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ({ className, variant, size, asChild = false, ...props }, ref) => {
         const Comp = asChild ? Slot : 'button';
         return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
-    }
+    },
 );
 Button.displayName = 'Button';
 
