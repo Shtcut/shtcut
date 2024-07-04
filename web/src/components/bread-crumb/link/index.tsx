0216f158c8e9a956385e623c60582f9ed6992497
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2024-07-04 22:40:47 +0100

    ui design on links dashboard , create link, preview link and edit link

--- web/src/components/bread-crumb/link/index.tsx
+++ web/src/components/bread-crumb/link/index.tsx
@@ -3,18 +3,23 @@ import { ChevronRight } from 'lucide-react';
 import { Button } from '@shtcut-ui/react';
 import { useRouter } from 'next/navigation';
 
-const LinkBreadCrumb = ({ currentRoute }: { currentRoute: string }) => {
-    const route = useRouter();
-    function HandleBavigateBack() {
-        route.back();
-    }
+const LinkBreadCrumb = ({ currentRoute, goBack }: { currentRoute: string; goBack?: () => void }) => {
+    const router = useRouter();
+
+    const back = () => {
+        if (goBack) {
+            goBack();
+        } else {
+            router.back();
+        }
+    };
     return (
         <div className="flex items-center gap-x-2 ">
             <Button variant={'unstyled'} className="text-[#898384] p-0  text-sm">
                 Home
             </Button>{' '}
             <ChevronRight size={16} color="#898384" />
-            <Button onClick={HandleBavigateBack} variant={'unstyled'} className="text-[#898384] p-0  text-sm">
+            <Button onClick={back} variant={'unstyled'} className="text-[#898384] p-0  text-sm">
                 Links
             </Button>
             <ChevronRight size={16} color="#898384" />
