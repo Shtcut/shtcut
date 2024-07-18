ed2e5b123f89877243f0b2bffa042dcbee2e50af
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2024-07-18 17:06:57 +0100

    update on auth ui adjustment

--- packages/react/src/components/ui/tabs.tsx
+++ packages/react/src/components/ui/tabs.tsx
@@ -14,8 +14,8 @@ const TabsList = React.forwardRef<
     <TabsPrimitive.List
         ref={ref}
         className={cn(
-            'inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground',
-            className
+            'inline-flex h-9 items-center justify-center rounded-lg bg-muted p-[2px] border border-gray-200 text-muted-foreground',
+            className,
         )}
         {...props}
     />
@@ -30,7 +30,7 @@ const TabsTrigger = React.forwardRef<
         ref={ref}
         className={cn(
             'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
-            className
+            className,
         )}
         {...props}
     />
@@ -45,7 +45,7 @@ const TabsContent = React.forwardRef<
         ref={ref}
         className={cn(
             'mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-            className
+            className,
         )}
         {...props}
     />
