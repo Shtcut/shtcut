e848863f2a99b11da45cd81a401608ef63e8ebf3
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2024-07-26 12:53:31 +0100

    unused div removal

--- web/src/containers/waitlist/index.tsx
+++ web/src/containers/waitlist/index.tsx
@@ -2,11 +2,7 @@ import WaitListComponent from '@shtcut/components/ui/waitlist';
 import React from 'react';
 
 const WaitListContainer = () => {
-    return (
-        <div>
-            <WaitListComponent />
-        </div>
-    );
+    return <WaitListComponent />;
 };
 
 export default WaitListContainer;
