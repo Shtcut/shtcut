9f442fb37984456fc82f20185a705caef8b76040
funmiayinde <funmiayinde11@gmail.com>
2024-09-06 14:46:18 +0100

    remove unused variables

--- packages/react/src/utils/component.ts
+++ packages/react/src/utils/component.ts
@@ -1,4 +1,4 @@
-import { ComponentPropsWithRef, ElementType, ReactElement, forwardRef, Ref } from 'react';
+import { ComponentPropsWithRef, ElementType, ReactElement, forwardRef } from 'react';
 import { Merge } from '../types';
 
 export type PrimitiveComponentProps<C extends ElementType = 'div', Props = {}> = Merge<ComponentPropsWithRef<C>, Props>;
