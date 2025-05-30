d930c163d993f235a27ca1120eefde3d2831fea9
nwokoyepraise <nwokoyepraise@gmail.com>
2025-05-30 16:36:47 +0100

    fix: remove unused prop

--- backend/apps/sht-shtner/src/link-bio/service/link-bio.service.ts
+++ backend/apps/sht-shtner/src/link-bio/service/link-bio.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Hit, HitDocument, LinkBio, LinkBioDocument, MongoBaseService } from 'shtcut/core';
+import { LinkBio, LinkBioDocument, MongoBaseService } from 'shtcut/core';
 
 @Injectable()
 export class LinkBioService extends MongoBaseService {
-  constructor(
-    @InjectModel(LinkBio.name) protected model: Model<LinkBioDocument>,
-    @InjectModel(Hit.name) protected hitModel: Model<HitDocument>,
-  ) {
+  constructor(@InjectModel(LinkBio.name) protected model: Model<LinkBioDocument>) {
     super(model);
   }
 
