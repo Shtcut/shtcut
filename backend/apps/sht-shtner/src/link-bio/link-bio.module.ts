b1cd0823681c9bde8b17ee83b822bb192eb2af79
nwokoyepraise <nwokoyepraise@gmail.com>
2025-06-10 23:45:33 +0100

    feat: improve links definition in link-bio

--- backend/apps/sht-shtner/src/link-bio/link-bio.module.ts
+++ backend/apps/sht-shtner/src/link-bio/link-bio.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { LinkBioController } from './controller/link-bio.controller';
 import { LinkBioService } from './service/link-bio.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Hit, HitSchema, LinkBio, LinkBioSchema, Workspace, WorkspaceSchema } from 'shtcut/core';
+import { Hit, HitSchema, LinkBio, LinkBioSchema, Media, MediaSchema, Workspace, WorkspaceSchema } from 'shtcut/core';
 import { HitModule } from '../hit';
 
 @Module({
@@ -11,6 +11,7 @@ import { HitModule } from '../hit';
       { name: LinkBio.name, schema: LinkBioSchema },
       { name: Workspace.name, schema: WorkspaceSchema },
       { name: Hit.name, schema: HitSchema },
+      { name: Media.name, schema: MediaSchema },
     ]),
     HitModule,
   ],
