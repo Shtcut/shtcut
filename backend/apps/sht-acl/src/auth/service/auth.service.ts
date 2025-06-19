7e5dd7f26e6219dfb52eea09a4efbf04ba327ccc
funmiayinde <funmiayinde11@gmail.com>
2025-06-19 13:17:38 +0100

    Fixed: Remove th

--- backend/apps/sht-acl/src/auth/service/auth.service.ts
+++ backend/apps/sht-acl/src/auth/service/auth.service.ts
@@ -315,8 +315,6 @@ export class AuthService extends MongoBaseService {
   }
 
   public getCode() {
-    return this.config.get('environment') === 'production'
-      ? Utils.generateCode(6)
-      : this.config.get('app.defaultVerifyCode');
+    return Utils.generateCode(6);
   }
 }
