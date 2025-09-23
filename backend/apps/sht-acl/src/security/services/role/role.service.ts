0ba4d1ac2af5c23fc3f6228b957de23e8d4ff01d
funmiayinde <funmiayinde11@gmail.com>
2025-09-24 00:29:36 +0100

    Fix: Added queue name for social media integration

--- backend/apps/sht-acl/src/security/services/role/role.service.ts
+++ backend/apps/sht-acl/src/security/services/role/role.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { MongoBaseService, Role, RoleDocument } from 'shtcut/core';
+import { Request } from 'express';
+import { ClientSession, Model } from 'mongoose';
+import { Dict, MongoBaseService, Role, RoleDocument } from 'shtcut/core';
 
 @Injectable()
 export class RoleService extends MongoBaseService {
   constructor(@InjectModel(Role.name) protected model: Model<RoleDocument>) {
     super(model);
+  }
+
+  public async createNewObject(obj: Dict, session?: ClientSession, req?: Request) {
+    obj.workspace = String(obj.workspace);
+    return super.createNewObject(obj, session, req);
   }
 }
